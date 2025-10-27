11d984aca2c0e750aa1dae59be9cc3e2ec3e9cfc
SugusGamberra <125316695+SugusGamberra@users.noreply.github.com>
2025-10-27 01:03:51 +0100

    Curso 2: Variables

---
+++ 0. Fundamentos de la Programación en JAVA/2. Variables/Variables/src/vpackage/Variables.java
@@ -0,0 +1,23 @@
+package vpackage;
+
+public class Variables {
+
+	public static void main(String[] args) {
+		// Vamos a crear una tanda de variables!
+		
+		String nombre = "Illidan";
+		int edad = 1;
+		double precio = 5.99;
+		char simbolo = '$';
+		
+		// Vaamos a mostrar una variable en consola!
+		
+		System.out.println(nombre);
+		
+		// Y ahora vamos a concatenar ;3
+		
+		System.out.println("Holaaa! Me llamo " + nombre + " y tengo menos de " + edad + " años... Mi mami me compró unas gotitas para mi ojito enfermo que costaron " + precio + simbolo);
+
+	}
+
+}
